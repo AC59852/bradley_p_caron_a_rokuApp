d74a8d9a5c90cc2229d06239769bd49df6dc12ab
AC59852 <austin.caron1@gmail.com>
2020-03-08 18:52:07 -0400

    added movies functionality

--- js/components/AdultsComponent.js
+++ js/components/AdultsComponent.js
@@ -1,11 +1,40 @@
+import SingleMovieComponent from "./SingleMovieComponent.js";
+
+// loop through the profiles and render each one
 export default {
     template: `
     <div class="container">
-        <div class="adultWrapper">
-            <div>
-                <h1>this is the Adults Component</h1>
+        <div class="">
+            <div class="movieWrapper">
+                <movies v-for="movie in movies" :key="movie.id" :movies="movie"></movies>
             </div>
         </div>
-    </div>
-    `
+    </div> 
+    `, 
+
+    data: function() {
+        return {
+            movies: []
+        }
+    },
+
+    created: function() {
+        // render the profiles on the page
+        this.fetchAllMovies();
+    },
+
+    methods: {
+        fetchAllMovies() {
+            let url = `./includes/index.php?getAllMovies=true`;
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {this.movies = data})
+            .catch((err) => {console.error(err)})
+        }
+    },
+
+    components: {
+        // grab any profiles from singleuser component
+        movies: SingleMovieComponent
+    }
 }
---
+++ js/components/SingleMovieComponent.js
@@ -0,0 +1,17 @@
+// this will load a profile that corresponds to whether or not the profile is labeled as kids
+export default {
+    props: ['movies'], 
+
+    // this will check whether the profile is that of a kids or not. if so, set the link to /kids
+    // if not, set it to /adults
+
+    template: `
+    <div class="movieContainer">
+    <img :src="'images/' + movies.image + '.jpg'" alt="movie banner" class="movieImg"> 
+        <h2>{{ movies.name }}</h2>
+    </div>
+    `  
+}
+
+  
+    
