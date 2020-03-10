4254e23bedd0d600889ee6ede14f6f75f527af64
AC59852 <austin.caron1@gmail.com>
2020-03-10 14:51:03 -0400

    added favicon code cleanup

--- js/components/SingleMovieComponent.js
+++ js/components/SingleMovieComponent.js
@@ -1,9 +1,6 @@
-// this will load a profile that corresponds to whether or not the profile is labeled as kids
+// this is the template for each movie rendered using v-for
 export default {
     props: ['movies'], 
-
-    // this will check whether the profile is that of a kids or not. if so, set the link to /kids
-    // if not, set it to /adults
 
     template: `
     <div class="movieContainer contentCon">
